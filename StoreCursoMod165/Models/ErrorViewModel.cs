dfbb916a2ec61ce93eaf1319e32ea20056087b75
AnaBernard0 <bernardo.catarina32@gmail.com>
2024-05-17 00:07:38 +0100

    Add project files.

---
+++ StoreCursoMod165/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreCursoMod165.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
