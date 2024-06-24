e07a62e1b18cd6a461a4c17d260af74c9a654ec5
AnaBernard0 <bernardo.catarina32@gmail.com>
2024-06-24 11:39:15 +0100

    Add Views Sent, EditInProgress, EditProcessed

--- StoreCursoMod165/Controllers/OrderController.cs
+++ StoreCursoMod165/Controllers/OrderController.cs
@@ -46,11 +46,20 @@ namespace StoreCursoMod165.Controllers
             if (ModelState.IsValid)
             {
                 this.SetUpOrderModel();
-                
-                
+                var products = _context.Products.ToList();
+                decimal price = 0;
+                //Calcula o preco da venda de acordo com os produtos seliconados
+                foreach(var xy in products)
+                {
+                    if(xy.ID==order.ProductID)
+                        price=xy.Price;
+                }
+               
                 //Criar new category
-                
+                order.TotalValue = Decimal.Parse(order.Quantity) * (price);
                 _context.Orders.Add(order);
+                
+               // 
                 _context.SaveChanges();
                
 
@@ -91,35 +100,6 @@ namespace StoreCursoMod165.Controllers
             return View(order);
         }
 
-        [HttpGet]
-        public IActionResult SeeOrders(Customer customer)
-        {
-            if (customer == null)
-                return NotFound();
-            var seeOrdersbyCustomer = _context.Orders
-                                               .Include(o=>o.Product)
-                                               .Include(o => o.Status)
-                                               .Include(o=>o.Customer)
-                                               .Where(o=>o.CustomerID == customer.ID)
-                                               .ToList();
-            this.SetUpOrderModel();
-            var name = _context.Orders.Include(o => o.Customer).Where(o => o.CustomerID == customer.ID).FirstOrDefault();
-            ViewBag.Name = name.Customer.Name;
-
-            decimal totalPago = 0;
-            decimal totalPorPagar = 0;
-            foreach (var order in seeOrdersbyCustomer)
-            {
-                if(order.IsPaid)
-                    totalPago=totalPago+order.TotalValue;
-                totalPorPagar=totalPorPagar+order.TotalValue;
-            }
-
-            ViewBag.TotalPaid=totalPago;
-            ViewBag.TotalPor=totalPorPagar;
-            ViewBag.Soma=totalPago-totalPorPagar;
-            return View(seeOrdersbyCustomer);
-        }
 
         [HttpPost]
         public IActionResult Edit(Order order)
@@ -168,6 +148,36 @@ namespace StoreCursoMod165.Controllers
         }
 
         [HttpGet]
+        public IActionResult SeeOrders(Customer customer)
+        {
+            if (customer == null)
+                return NotFound();
+            var seeOrdersbyCustomer = _context.Orders
+                                               .Include(o => o.Product)
+                                               .Include(o => o.Status)
+                                               .Include(o => o.Customer)
+                                               .Where(o => o.CustomerID == customer.ID)
+                                               .ToList();
+            this.SetUpOrderModel();
+            var name = _context.Orders.Include(o => o.Customer).Where(o => o.CustomerID == customer.ID).FirstOrDefault();
+            ViewBag.Name = name.Customer.Name;
+
+            decimal totalPago = 0;
+            decimal totalPorPagar = 0;
+            foreach (var order in seeOrdersbyCustomer)
+            {
+                if (order.IsPaid)
+                    totalPago = totalPago + order.TotalValue;
+                totalPorPagar = totalPorPagar + order.TotalValue;
+            }
+
+            ViewBag.TotalPaid = totalPago;
+            ViewBag.TotalPor = totalPorPagar;
+            ViewBag.Soma = totalPago - totalPorPagar;
+            return View(seeOrdersbyCustomer);
+        }
+
+        [HttpGet]
         public IActionResult Ordered()
         {
             var seeOrdered = _context.Orders
@@ -181,6 +191,149 @@ namespace StoreCursoMod165.Controllers
             return View(seeOrdered);
         }
 
+        [HttpGet]
+        public IActionResult InProgress()
+        {
+            var inprogress = _context.Orders
+                                               .Include(o => o.Product)
+                                               .Include(o => o.Status)
+                                               .Include(o => o.Customer)
+                                               .Where(o => o.StatusID == 2)
+                                               .ToList();
+
+            
+            this.SetUpOrderModel();
+
+            return View(inprogress);
+        }
+
+        [HttpGet]
+        public IActionResult EditInProgress(int id)
+        {
+            Order? order = _context.Orders
+                                           .Include(o => o.Product)
+                                           .Include(o => o.Customer)
+                                           .Include(o => o.Status)
+                                           .Where(o => o.ID == id)
+                                           .Single();
+
+
+            if (order == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            this.SetUpOrderModel();
+            return View(order);
+        }
+        [HttpPost]
+        public IActionResult EditInProgress(Order order)
+        {
+             order = _context.Orders
+                                           .Include(o => o.Product)
+                                           .Include(o => o.Customer)
+                                           .Include(o => o.Status)
+                                           .Where(o => o.ID==order.ID)
+                                           .Single();
+
+
+            if (order == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else 
+            {
+                    //Verifica a existencia do produto, se nao existir abate o preco para 0 
+                    if (order.StatusID == 2 && ((Convert.ToInt32(order.Product.Quantity)) - (Convert.ToInt32(order.Quantity)) < 0))
+                    {
+                        order.Informations = "There is no more " + order.Product.Description;
+                        order.TotalValue = 0;
+
+                    }    
+                    else
+                       order.StatusID = 3;
+                
+            }
+            //Guarda a encomenda como processada
+            _context.SaveChanges();
+            this.SetUpOrderModel();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Processed()
+        {
+            var seeOrdered = _context.Orders
+                                               .Include(o => o.Product)
+                                               .Include(o => o.Status)
+                                               .Include(o => o.Customer)
+                                               .Where(o => o.StatusID == 3)
+                                               .ToList();
+            this.SetUpOrderModel();
+
+            return View(seeOrdered);
+        }
+
+        [HttpGet]
+        public IActionResult EditProcessed(int id)
+        {
+            Order? order = _context.Orders
+                                           .Include(o => o.Product)
+                                           .Include(o => o.Customer)
+                                           .Include(o => o.Status)
+                                           .Where(o => o.ID == id)
+                                           .Single();
+
+
+            if (order == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            this.SetUpOrderModel();
+            return View(order);
+        }
+        [HttpPost]
+        public IActionResult EditProcessed(Order order)
+        {
+
+            if (order == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                //Atualiza stocks dos produtos
+                if (order.StatusID == 3)
+                {
+                    order = _context.Orders
+                                                   .Include(o => o.Product)
+                                                   .Include(o => o.Customer)
+                                                   .Where(o => o.ID == order.ID)
+                                                   .Single();
+                    order.Product.Quantity = Convert.ToString(Convert.ToInt32(order.Product.Quantity) - Convert.ToInt32(order.Quantity));
+                    order.StatusID = 4;
+                }
+                //ENVIA EMAIL AO CLIENTE FINAL
+            }
+            _context.SaveChanges();
+            this.SetUpOrderModel();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Sent()
+        {
+            var inprogress = _context.Orders
+                                               .Include(o => o.Product)
+                                               .Include(o => o.Status)
+                                               .Include(o => o.Customer)
+                                               .Where(o => o.StatusID == 4)
+                                               .ToList();
+
+
+            this.SetUpOrderModel();
+
+            return View(inprogress);
+        }
         private void SetUpOrderModel()
         {
             ViewBag.ProductList = new SelectList(_context.Products, "ID", "Description");
